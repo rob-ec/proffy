3d4e120d2833d3b3874d9b04609c4377df07f815
Robson M. Gomes <robson.mesquita56@gmail.com>
2020-08-08 15:26:28 -0300

    feat: Add Formfileds to TeacherForm
    Add useful form to TeacherForm Page by creating new components

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,15 +1,102 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import PageHeader from '../../components/PageHeader'
+import PageHeader from "../../components/PageHeader";
+import Input from "../../components/Input";
 
-import './styles.css'
+import warningIcon from "../../assets/images/icons/warning.svg";
+
+import "./styles.css";
+import Textarea from "../../components/TextArea";
+import Select from "../../components/Select";
 
 function TeacherForm() {
-    return(
-        <div id="teacher-form">
-            <PageHeader title="Que bom que você quer dar aulas!" />
-        </div>
-    );
+  const [scheduleItems, setScheduleItems] = useState([
+    { week_day: 0, from: '', to: '' }
+  ]);
+
+  function newScheduleItem() {
+    setScheduleItems([
+      ...scheduleItems,
+      {
+        week_day: 0,
+        from: "",
+        to: "",
+      },
+    ]);
+  }
+
+  return (
+    <div id="page-teacher-form">
+      <PageHeader
+        title="Que bom que você quer dar aulas!"
+        description="O primeiro passo é preencher esse formulário de inscrição."
+      />
+      <main>
+        <fieldset>
+          <legend>Seus Dados:</legend>
+          <Input name="name" label="Nome Completo" />
+          <Input name="avatar" label="Avatar" />
+          <Input name="whatsapp" label="Whatsapp" />
+          <Textarea name="bio" label="Biografia" />
+        </fieldset>
+        <fieldset>
+          <legend>Sobre a Aula:</legend>
+          <Select
+            name="subject"
+            label="Matéria"
+            options={[
+              { value: "Artes", label: "Artes" },
+              { value: "Biologia", label: "Biologia" },
+              { value: "Matemática", label: "Matemática" },
+              { value: "Português", label: "Português" },
+              { value: "História", label: "História" },
+            ]}
+          />
+          <Input name="cost" label="Custo da sua hora por aula" />
+        </fieldset>
+
+        <fieldset>
+          <legend>
+            Horários disponíveis
+            <button type="button" onClick={newScheduleItem}>
+              + Novo horário
+            </button>
+          </legend>
+          {scheduleItems.map((scheduleItem) => {
+            return (
+              <div key={scheduleItem.week_day} className="schedule-item">
+                <Select
+                  name="week_day"
+                  label="Dia da semana"
+                  options={[
+                    { value: "0", label: "Domingo" },
+                    { value: "1", label: "Segunda-feira" },
+                    { value: "2", label: "Terça-feira" },
+                    { value: "3", label: "Quarta-feira" },
+                    { value: "4", label: "Quinta-feira" },
+                    { value: "5", label: "Sextaj-feira" },
+                    { value: "6", label: "Sábado-feira" },
+                  ]}
+                />
+                <Input name="from" label="Das" type="time" />
+                <Input name="to" label="Até" type="time" />
+              </div>
+            );
+          })}
+        </fieldset>
+
+        <footer>
+          <p>
+            <img src={warningIcon} alt="Aviso importante" />
+            Importante!
+            <br />
+            preencha todos os dados.
+          </p>
+          <button type="button">Salvar Cadastro</button>
+        </footer>
+      </main>
+    </div>
+  );
 }
 
 export default TeacherForm;
